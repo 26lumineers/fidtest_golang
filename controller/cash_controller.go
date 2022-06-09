3242bbde24ab579a1d74b415c6c06b99a34de064
Fid <fidiiz002@gmail.com>
2022-06-09 09:03:25 +0700

    fix bug and clean code with better comment

--- controller/cash_controller.go
+++ controller/cash_controller.go
@@ -13,20 +13,20 @@ import (
 func Cashier(w http.ResponseWriter, r *http.Request) {
 
 	product_price, err1 := strconv.ParseFloat(r.FormValue("product_price"), 64)
-	if err1 != nil {
+	if err1 != nil { // if product price is not number type ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณาตั้งราคาเป็นตัวเลขเท่านั้น!")
 		return
 	}
 	cash, err2 := strconv.ParseFloat(r.FormValue("cash"), 64)
-	if err2 != nil {
+	if err2 != nil { // if cash is not number type ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณากรอกเงินที่จ่ายเป็นตัวเลขเท่านั้น!")
 		return
 	}
-	if math.Mod(product_price, 0.25) != 0 {
+	if math.Mod(product_price, 0.25) != 0 { // if product price didn't end with 0.25,0.75,0.50 ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณาตั้งราคาสินค้าใหม่,เนื่องจากเหรียญที่สามารถใช้ได้ 10,5,1,25สตางค์")
 		return
 	}
-	if math.Mod(cash, 0.25) != 0 {
+	if math.Mod(cash, 0.25) != 0 { // if cash didn't end with 0.25,0.75,0.50 ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณากรอกเงินที่จ่ายให้ถูกต้อง,เหรียญที่สามารถใช้ได้ 10,5,1,25สตางค์")
 		return
 	}
@@ -42,7 +42,7 @@ func Cashier(w http.ResponseWriter, r *http.Request) {
 	var change_1 int
 	var change_025 int
 	var remainder float64
-	_=remainder
+	_ = remainder
 	//find total of bank note
 	for k := range bank_note {
 		total_bank_note += k * float64(bank_note[k])
@@ -52,16 +52,16 @@ func Cashier(w http.ResponseWriter, r *http.Request) {
 	if cash == product_price {
 		model.ModelResponseText(w, http.StatusOK, "ขอบคุณที่ใช้บริการ")
 		return
-	} else if cash < product_price {
+	} else if cash < product_price { // if cash is less than product price, it means that you can't buy this product and return response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "ยอดเงินชำระไม่เพียงพอ กรุณาชำระใหม่")
 		return
-	} else if (cash - product_price) > total_bank_note {
+	} else if (cash - product_price) > total_bank_note { //if change is too much more than total_banknote is available on deck, it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "ขออภัย ขณะนี้ธนบัตรไม่เพียงพอในการทอนเงิน,โปรดชำระอีกครั้ง")
 		return
 	} else if cash > product_price {
 		change := math.Round((cash-product_price)*100) / 100
 		fmt.Println("change : ", change)
-		
+		//if change is > 0 we will calculate the best way to return change to customer as condition limit of bank note
 		if change > 0 {
 			if int(change/1000) > 10 {
 				change_1000 = 10
@@ -180,22 +180,22 @@ func Cashier(w http.ResponseWriter, r *http.Request) {
 }
 
 // this function just in case for no codition for limiting existing banknotes
-func Cashier_infinity(w http.ResponseWriter, r *http.Request){
+func Cashier_infinity(w http.ResponseWriter, r *http.Request) {
 	product_price, err1 := strconv.ParseFloat(r.FormValue("product_price"), 64)
-	if err1 != nil {
+	if err1 != nil { // if product price is not number type ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณาตั้งราคาเป็นตัวเลขเท่านั้น!")
 		return
 	}
 	cash, err2 := strconv.ParseFloat(r.FormValue("cash"), 64)
-	if err2 != nil {
+	if err2 != nil { // if cash is not number type ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณากรอกเงินที่จ่ายเป็นตัวเลขเท่านั้น!")
 		return
 	}
-	if math.Mod(product_price, 0.25) != 0 {
+	if math.Mod(product_price, 0.25) != 0 { // if product price didn't end with 0.25,0.75,0.50 ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณาตั้งราคาสินค้าใหม่,เนื่องจากเหรียญที่สามารถใช้ได้ 10,5,1,25สตางค์")
 		return
 	}
-	if math.Mod(cash, 0.25) != 0 {
+	if math.Mod(cash, 0.25) != 0 { // if cash didn't end with 0.25,0.75,0.50 ,it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "กรุณากรอกเงินที่จ่ายให้ถูกต้อง,เหรียญที่สามารถใช้ได้ 10,5,1,25สตางค์")
 		return
 	}
@@ -211,7 +211,7 @@ func Cashier_infinity(w http.ResponseWriter, r *http.Request){
 	var change_1 int
 	var change_025 int
 	var remainder float64
-	_=remainder
+	_ = remainder
 	//find total of bank note
 	for k := range bank_note {
 		total_bank_note += k * float64(bank_note[k])
@@ -219,56 +219,63 @@ func Cashier_infinity(w http.ResponseWriter, r *http.Request){
 
 	//if cash is equal to product price, we don't need to return change.
 	if cash == product_price {
-		model.ModelResponseText(w, http.StatusOK, "ชำระเงินสำเร็จ")
+		model.ModelResponseText(w, http.StatusOK, "ขอบคุณที่ใช้บริการ")
 		return
-	} else if cash < product_price {
+	} else if cash < product_price { // if cash is less than product price, it means that you can't buy this product and return response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "ยอดเงินชำระไม่เพียงพอ กรุณาชำระใหม่")
 		return
-	} else if (cash - product_price) > total_bank_note {
+	} else if (cash - product_price) > total_bank_note { //if change is too much more than total_banknote is available on deck, it will throw the response as 400
 		model.ModelResponseText(w, http.StatusBadRequest, "ขออภัย ขณะนี้ธนบัตรไม่เพียงพอในการทอนเงิน,โปรดชำระอีกครั้ง")
 		return
 	} else if cash > product_price {
 		change := math.Round((cash-product_price)*100) / 100
 		fmt.Println("change : ", change)
 		if change > 0 {
-		
-				change_1000 = int(change / 1000)
-				change -= float64(change_1000 * 1000)
-				remainder = math.Mod(change, 1000)
-				total_bank_note = total_bank_note - (float64(change_1000) * 1000)
-				change_500 = int(change / 500)
-				change -= float64(change_500 * 500)
-				remainder = math.Mod(change, 500)
-				total_bank_note = total_bank_note - (float64(change_500) * 500)
-		
-				change_100 = int(change / 100)
-				change -= float64(change_100 * 100)
-				remainder = math.Mod(change, 100)
-				total_bank_note = total_bank_note - (float64(change_500) * 100)
-				change_50 = int(change / 50)
-				change -= float64(change_50 * 50)
-				remainder = math.Mod(change, 50)
-				total_bank_note = total_bank_note - (float64(change_50) * 500)
-				change_20 = int(change / 20)
-				change -= float64(change_20 * 20)
-				remainder = math.Mod(change, 20)
-				total_bank_note = total_bank_note - (float64(change_20) * 20)
-				change_10 = int(change / 10)
-				change -= float64(change_10 * 10)
-				remainder = math.Mod(change, 10)
-				total_bank_note = total_bank_note - (float64(change_10) * 10)
-				change_5 = int(change / 5)
-				change -= float64(change_5 * 5)
-				remainder = math.Mod(change, 5)
-				total_bank_note = total_bank_note - (float64(change_5) * 5)
-				change_1 = int(change / 1)
-				change -= float64(change_1)
-				remainder = math.Mod(change, 1)
-				total_bank_note = total_bank_note - float64(change_1)
-				change_025 = int(change / 0.25)
-				change -= float64(change_025)
-				remainder = math.Mod(change, 0.25)
-				total_bank_note = total_bank_note - (float64(change_025) * 0.25)
+			//calculate for 1000 banknotes
+			change_1000 = int(change / 1000)
+			change -= float64(change_1000 * 1000)
+			remainder = math.Mod(change, 1000)
+			total_bank_note = total_bank_note - (float64(change_1000) * 1000)
+			//calculate for 500 banknotes
+			change_500 = int(change / 500)
+			change -= float64(change_500 * 500)
+			remainder = math.Mod(change, 500)
+			total_bank_note = total_bank_note - (float64(change_500) * 500)
+			//calculate for 100 banknotes
+			change_100 = int(change / 100)
+			change -= float64(change_100 * 100)
+			remainder = math.Mod(change, 100)
+			total_bank_note = total_bank_note - (float64(change_500) * 100)
+			//calculate for 50 banknotes
+			change_50 = int(change / 50)
+			change -= float64(change_50 * 50)
+			remainder = math.Mod(change, 50)
+			total_bank_note = total_bank_note - (float64(change_50) * 50)
+			//calculate for 20 banknotes
+			change_20 = int(change / 20)
+			change -= float64(change_20 * 20)
+			remainder = math.Mod(change, 20)
+			total_bank_note = total_bank_note - (float64(change_20) * 20)
+			//calculate for 10 banknotes
+			change_10 = int(change / 10)
+			change -= float64(change_10 * 10)
+			remainder = math.Mod(change, 10)
+			total_bank_note = total_bank_note - (float64(change_10) * 10)
+			//calculate for coin 5
+			change_5 = int(change / 5)
+			change -= float64(change_5 * 5)
+			remainder = math.Mod(change, 5)
+			total_bank_note = total_bank_note - (float64(change_5) * 5)
+			//calculate for coin 1
+			change_1 = int(change / 1)
+			change -= float64(change_1)
+			remainder = math.Mod(change, 1)
+			total_bank_note = total_bank_note - float64(change_1)
+			//calculate for coin 0.25
+			change_025 = int(change / 0.25)
+			change -= float64(change_025)
+			remainder = math.Mod(change, 0.25)
+			total_bank_note = total_bank_note - (float64(change_025) * 0.25)
 			payload := entity.CashInfo{
 				Cash_1000: change_1000,
 				Cash_500:  change_500,
